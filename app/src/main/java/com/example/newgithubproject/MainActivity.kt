4bbe5dad78415961fc0329d108edf14845a150b5
GCA 1 <muhammadhamza664@gmail.com>
2020-02-05 17:49:04 +0500

    first commit1

--- app/src/main/java/com/example/newgithubproject/MainActivity.kt
+++ app/src/main/java/com/example/newgithubproject/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.newgithubproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this,"hello commit",Toast.LENGTH_SHORT)
     }
 }
